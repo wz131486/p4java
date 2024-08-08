acb0bc0553c4961cad767c09acc726cd0f76e98a
zhen.wan <zhen.wan@smartereye.com>
2024-08-08 09:15:54 +0800

    feat: add ch01 code. hello world.

---
+++ src/com/wz/se/ch01/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.wz.se.ch01;
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World ... ");
+	}
+}
---
+++ src/com/wz/se/ch01/HelloWorld666.java
@@ -0,0 +1,10 @@
+package com.wz.se.ch01;
+
+public class HelloWorld666
+{ 
+	public static void main(String[] args)
+	{
+		System.out.println("Hello ...");
+	} 
+
+}
